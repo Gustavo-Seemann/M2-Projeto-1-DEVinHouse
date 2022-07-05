e8312173aee1dbd9d867bf7957109f529d4b2a9d
Gustavo-Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-07-04 21:42:52 -0300

    adiciona classe moto
    adicionei a classe filha moto.

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ class Veiculo:
         self.nome = nome
         self.placa = placa
         self.valor = valor
-        self.__cpf_comprador = cpf_comprador
+        self.__cpf_comprador = 0
         self.cor = cor
+
+class Moto(Veiculo):
+    def init(self, chassi: int, data_fabricado: str, nome: str, placa: str, valor: float, cpf_comprador: str, cor: str, potencia: int, numero_rodas: int):
+        super().__init__(chassi, data_fabricado, nome, placa, valor, cpf_comprador, cor)
+        self.potencia = potencia
+        self.numero_rodas = numero_rodas
