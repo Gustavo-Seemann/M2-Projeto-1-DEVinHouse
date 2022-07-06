62a68559bd69e1f8d1ff91e2d2354dccb79d51f6
Gustavo-Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-07-06 18:27:14 -0300

    update chassi
    adiciona id aleatorio para chassi

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import uuid
+
 class Veiculo:
     def __init__(self, chassi: int, data_fabricado: str, nome: str, placa: str, valor: float, cpf_comprador: str, cor: str):
-        self.__chassi = chassi
+        self.__chassi = uuid.uuid4() 
         self.data_fabricado = data_fabricado
         self.nome = nome
         self.placa = placa
