77940908fa448cae6d48d02b2f27d8655ea1da18
Gustavo-Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-07-07 06:33:37 -0300

    adiciona cadastro de veiculos
    adicionei a logica para cadastrar os veiculos

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from uuid import uuid4
 
-Veiculos = {'motos': 'teste', 'carros': 'teste2', 'camionete': 'teste3'}
+Veiculos = {'motos': '', 'carros': '', 'camionetes': ''}
 Motos = []
+Carros = []
+Camionetes = []
+
 
 
 class Veiculo:
@@ -28,6 +31,7 @@ class Veiculo:
         self.__cpf_comprador = cpf
 
 
+
 class Moto(Veiculo):
     def __init__(self, data_fabricado: str, nome: str, placa: str, valor: float, cor: str, potencia: int, numero_rodas: int):
         super().__init__(data_fabricado, nome, placa, valor, cor)
@@ -50,6 +54,13 @@ class Moto(Veiculo):
         Motos.append(moto)
         Veiculos['motos'] = Motos
 
+    @staticmethod
+    def MostrarMotos():
+        for motos in Veiculos['motos']:
+            print(f"Moto Modelo: {motos['nome']}, {motos['data_fabricado']}, de {motos['numero_rodas']} rodas, com a Potência de: {motos['potencia']} cavalos, Placa: {motos['placa']}, Cor: {motos['cor']}, possui o valor de R${motos['valor']}")
+
+
+
 class Carro(Veiculo):
     def __init__(self, data_fabricado: str, nome: str, placa: str, valor: float, cor: str, total_portas: int, modo: str, potencia: int):
         super().__init__(data_fabricado, nome, placa, valor, cor)
@@ -57,26 +68,87 @@ class Carro(Veiculo):
         self.modo = modo
         self.potencia = potencia
 
+    def CadastrarCarro(self):
+        carro = {
+            'data_fabricado': self.data_fabricado,
+            'nome': self.nome,
+            'placa': self.placa,
+            'valor': self.valor,
+            'cor': self.cor,
+            'total_portas': self.total_portas,
+            'modo': self.modo,
+            'potencia': self.potencia,
+            'chassi': self.chassi,
+            'cpf_comprador': self.cpf_comprador,
+            'vendido': self.vendido
+        }
+        Carros.append(carro)
+        Veiculos['carros'] = Carros
+
+    @staticmethod
+    def MostrarCarros():
+        for carros in Veiculos['carros']:
+            print(f"Carro Modelo: {carros['nome']}, {carros['data_fabricado']}, de {carros['total_portas']} de portas, Motor: {carros['modo']} com a Potência de: {carros['potencia']} cavalos, Placa: {carros['placa']}, Cor: {carros['cor']}, possui o valor de R${carros['valor']}")
+
+
 
 class Camionete(Veiculo):
-    def __init__(self, data_fabricado: str, nome: str, placa: str, valor: float, total_portas: int, cap_cacamba: int, potencia: int, modo: str ):
-        super().__init__(data_fabricado, nome, placa, valor)
-        self.cor = "Roxa"
+    def __init__(self, data_fabricado: str, nome: str, placa: str, valor: float, total_portas: int, cap_cacamba: int, potencia: int, modo: str, cor = "Roxa"):
+        super().__init__(data_fabricado, nome, placa, valor, cor)
         self.total_portas = total_portas
         self.cap_cacamba = cap_cacamba
         self.potencia = potencia
         self.modo = modo
 
-def MostrarMotos():
-    for motos in Veiculos['motos']:
-        print(motos['nome'])
+    def CadastrarCamionetes(self):
+        camionete = {
+            'data_fabricado': self.data_fabricado,
+            'nome': self.nome,
+            'placa': self.placa,
+            'valor': self.valor,
+            'cor': self.cor,
+            'total_portas': self.total_portas,
+            'modo': self.modo,
+            'cap_cacamba': self.cap_cacamba,
+            'potencia': self.potencia,
+            'chassi': self.chassi,
+            'cpf_comprador': self.cpf_comprador,
+            'vendido': self.vendido
+        }
+        Camionetes.append(camionete)
+        Veiculos['camionetes'] = Camionetes
+
+    @staticmethod
+    def MostrarCamionetes():
+        for camionetes in Veiculos['camionetes']:
+            print(f"Camionete Modelo: {camionetes['nome']}, {camionetes['data_fabricado']}, Motor: {camionetes['modo']} com a Potência de: {camionetes['potencia']} cavalos, Caçamba com capacidade de {camionetes['cap_cacamba']} litros, Placa: {camionetes['placa']}, Cor: {camionetes['cor']}, possui o valor de R$ {camionetes['valor']}")
 
 
-moto1 = Moto("14/04/2019", "Harley Davidson", "39AKB1", 39000, "Vermelha", 1000, 2)
+
+moto1 = Moto("2019", "Harley Davidson", "39AKB1", 39000, "Vermelha", 100, 2)
 moto1.CadastrarMoto()
-moto2 = Moto("14/04/2020", "Yamaha", "399BB1", 39300, "Azul", 1340, 3)
+moto2 = Moto("2020", "Yamaha", "399BB1", 39300, "Azul", 130, 3)
 moto2.CadastrarMoto()
-MostrarMotos()
+moto3 = Moto("1999", "Honda", "319AZB", 15490, "Preta", 90, 2)
+moto3.CadastrarMoto()
+Moto.MostrarMotos()
+
+carro1 = Carro("1969", "Ford Mustang", "B9318C", 278000, "Preto", 2, "Gasolina", 483)
+carro1.CadastrarCarro()
+carro2 = Carro("1990", "Ford Mustang GT", "LC319C", 385000, "Cinza", 2, "Gasolina", 490)
+carro2.CadastrarCarro()
+carro3 = Carro("2010", "Lamborghini", "C1C9LK", 560000, "Laranja", 2, "Flex", 433)
+carro3.CadastrarCarro()
+Carro.MostrarCarros()
+
+camionete1 = Camionete("2020", "Mitsubishi L200 Triton", "KCZ1CL", 145000, 4, 6000, 400, "Diesel")
+camionete1.CadastrarCamionetes()
+camionete2 = Camionete("2021", "Chevrolet S10 Z71", "L3Z1MC", 185000, 4, 4000, 410, "Gasolina")
+camionete2.CadastrarCamionetes()
+camionete3 = Camionete("2018", "Fiat Toro", "Z351CL", 193200, 4, 5500, 390, "Diesel")
+camionete3.CadastrarCamionetes()
+Camionete.MostrarCamionetes()
+
 print("RODOU!")
 
 
